a84e06155600f2508cc3a767cb70ed4cae5cd2b6
yasysn <yassine.lakdimi@student.ehb.be>
2024-05-24 17:21:35 +0200

    Event aan een element koppelen

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.getElementById('theme-switcher').addEventListener('click', function(){
     document.body.classList.toggle('dark-mode')
 })
+
+var scrollToTopButton = document.getElementById('scroll-to-top');
+
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 200){
+        scrollToTopButton.style.display = 'block';
+    }else {
+        scrollToTopButton.style.display = 'none';
+    }
+});
+
+scrollToTopButton.addEventListener('click', function(){
+    window.scrollTo({top: 0,behavior: 'smooth'});
+})
